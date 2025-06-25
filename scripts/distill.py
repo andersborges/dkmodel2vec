8b7f8d79d4aebacd9deb5aa95d3d5a357fc20fc6
Anders Borges <andersborges@hotmail.com>
2025-06-25 18:16:48 +0000

    add baseline script

--- scripts/distill.py
+++ scripts/distill.py
@@ -1,13 +1,23 @@
 from dkmodel2vec.llm_loader import load_llm2vec_model
+import torch
 
-# from model2vec.distill import distill_from_model
-
+from model2vec.distill import distill_from_model
+from dkmodel2vec.models import LlamaModelWrapper
+from pathlib import Path
 
 if __name__ == "__main__": 
     model = load_llm2vec_model()
+    wrapped_model = LlamaModelWrapper(model)
+    m2v_model = distill_from_model(
+        model=wrapped_model,
+        tokenizer=model.tokenizer,
+        pca_dims=256,
+    )    
 
-# Assuming a loaded model and tokenizer
+    models_dir = Path(__file__).parent / "models"
+    models_dir.mkdir(exist_ok=True)
 
-#    m2v_model = distill_from_model(model=model, tokenizer=tokenizer, pca_dims=256)
+    model_name =  "StaticDK_baseline"
+    m2v_model.save_pretrained(models_dir / model_name)
 
- #   m2v_model.save_pretrained("m2v_model")
+    
